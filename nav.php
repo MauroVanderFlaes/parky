129d7add9f739de9df7f2a2bfa3e610c15fdcf43
JarneSimons <r0901961@student.thomasmore.be>
2023-05-08 10:42:44 +0200

    feat: making nav bar

---
+++ nav.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Document</title>
+</head>
+<body>
+    <div class="bg-[#11211F] text-[#F2FFFF] h-20 fixed left-0 bottom-0 w-full z-0" >
+        <div class="flex justify-center gap-12 pt-2">
+
+            <div class="w-20">
+                <a href="#"><img src="img/navIcon1.png" alt="icon1"></a>   
+            </div>
+            <div class="w-20 relative bottom-1">
+                <a href="#"><img src="img/navIcon2.png" alt="icon2"></a>
+                
+            </div>
+            <div class="w-20" >  
+                <a href="#"><img src="img/navIcon3.png" alt="icon3"></a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
