1af0963568b204a51beb674e17f5b7d61e0c7915
JarneSimons <r0901961@student.thomasmore.be>
2023-05-16 21:22:11 +0200

    fix: changing name from firstcreen

---
+++ firstScreen.php
@@ -0,0 +1,34 @@
+<?php
+
+
+
+
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body class="bodyy">
+    
+    <div class="logoLogin"><img src="img/logo3.png" alt="foto"></div>
+    <div class="boxLogin">
+        <div class="inputBox">
+            <h2>welkom!</h2>
+            <p>maak een keuze</p>
+        </div>
+        <div class="formBtn">
+            <a class="inlogBtn" href="login.php"><p>inloggen</p></a>
+            <p>of</p>
+            <a class="inlogBtn2" href="registreer.php">registreren</a>
+        </div>
+
+        
+    </div>
+</body>
+</html>
