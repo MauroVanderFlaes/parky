0adc80a0f6fb25f6f9d4c5875b39f4d6a1fd2ba5
JarneSimons <r0901961@student.thomasmore.be>
2023-05-17 20:56:14 +0200

    feat: adding user class

---
+++ classes/User.php
@@ -0,0 +1,80 @@
+<?php 
+    class User{
+        private $username;
+        private $email;
+        private $password;
+
+
+        
+
+
+
+
+
+        /**
+         * Get the value of username
+         */ 
+        public function getUsername()
+        {
+                return $this->username;
+        }
+
+        /**
+         * Set the value of username
+         *
+         * @return  self
+         */ 
+        public function setUsername($username)
+        {
+                $this->username = $username;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of email
+         */ 
+        public function getEmail()
+        {
+                return $this->email;
+        }
+
+        /**
+         * Set the value of email
+         *
+         * @return  self
+         */ 
+        public function setEmail($email)
+        {
+                $this->email = $email;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of password
+         */ 
+        public function getPassword()
+        {
+                return $this->password;
+        }
+
+        /**
+         * Set the value of password
+         *
+         * @return  self
+         */ 
+        public function setPassword($password)
+        {
+                $this->password = $password;
+
+                return $this;
+        }
+    }
+
+
+
+
+
+
+?>
