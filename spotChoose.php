30cbf6af4d45adc0e1a92f30723f62062f9cb530
JarneSimons <r0901961@student.thomasmore.be>
2023-05-17 10:36:26 +0200

    feat: adding spotChoose file

---
+++ spotChoose.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Document</title>
+</head>
+
+<body>
+    <div id="logo_zwart"></div>
+    <div class="placement">
+        <div class="swiper-slide card">
+            <div class="card">
+                <div class="card-content">
+                    <div>
+                        <img src="" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+    <?php include 'nav.php'; ?>
+
+</body>
+<script src="script.js"></script>
+
+</html>
