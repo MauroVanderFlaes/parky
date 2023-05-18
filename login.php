706e4298f482ac36d795d40901283a05660bf331
JarneSimons <r0901961@student.thomasmore.be>
2023-05-18 17:18:12 +0200

    feat: making it possible to login after register

--- login.php
+++ login.php
@@ -1,5 +1,29 @@
-<?php 
+<?php
+include_once(__DIR__ . "/bootstrap.php");
+
+if(isset($_POST['login'])) {
     
+    if(!empty($_POST)) {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $user = new User();
+
+        if($user->canLogin($username, $password)) {
+            $_SESSION['loggedIn'] = true;
+            $_SESSION['id'] = $user->getIdByUsername($username);
+            header('Location: index.php');
+        } else {
+            $loginError = "Gebruikersnaam of wachtwoord is onjuist";
+        }
+    }
+}
+
+
+
+
+
+
+
 
 
 ?><!DOCTYPE html>
@@ -20,19 +44,26 @@
             <h2>inloggen</h2>
         </div>
         <div class="loginForm">
-            <form action="action" method="POST">
+            <form action="" method="post">
                 <p >gebruikersnaam</p>
                 <input type="text" name="username" id="username" placeholder="gebruikersnaam">
                 <p class="pswd">wachtwoord</p>
                 <input type="password" name="password" id="password" placeholder="wachtwoord">
+                <?php if(isset($loginError)): ?>
+                    <p><?php echo $loginError; ?></p> 
+                <?php endif; ?>
+
+
+                <div class="formBtnLogin">
+                    <!-- <a class="inlogBtn" href="login.php"><p>inloggen</p></a> -->
+                    <input class="inlogBtn" type="submit" value="inloggen" name="login" id=""></input>
+                </div>
+                <div class="lastLink">
+                    <a href="registreer.php">nog geen account? registreer hier</a>
+                </div>
+
             </form>
 
-        </div>
-        <div class="formBtnLogin">
-            <a class="inlogBtn" href="login.php"><p>inloggen</p></a>
-        </div>
-        <div class="lastLink">
-            <a href="registreer.php">nog geen account? registreer hier</a>
         </div>
 
         
