b0eead96b56761f192315587ca05112bbcb87934
JarneSimons <r0901961@student.thomasmore.be>
2023-05-18 15:29:18 +0200

    feat: adding bootstrap file for classes

---
+++ bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+    spl_autoload_register(function($class){
+        require_once(__DIR__ . DIRECTORY_SEPARATOR . "classes" . DIRECTORY_SEPARATOR . $class . ".php");
+    });
+    //autostart sessions
+    session_start();
