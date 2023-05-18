794edf9a6ebbd1ebd6a985f1a3d4a01439517b78
JarneSimons <r0901961@student.thomasmore.be>
2023-05-18 17:18:36 +0200

    bug: after register to index page

--- registreer.php
+++ registreer.php
@@ -27,7 +27,7 @@ if(isset($_POST['register'])) {
             
             $user->save();
             // var_dump($user);
-            header('Location: registreer.php');
+            header('Location: index.php');
 
 
         } catch (\Throwable $th) {
@@ -59,11 +59,19 @@ if(isset($_POST['register'])) {
             <form action="" method="post">
                 <p>gebruikersnaam</p>
                 <input type="text" name="username" id="username" placeholder="gebruikersnaam">
+                <?php if(isset($usernameError)): ?>
+                    <p><?php echo $usernameError; ?></p> 
+                <?php endif; ?>
                 <p class="registerEmail">emailadres</p>
                 <input type="text" name="email" id="email" placeholder="emailadres">
+                <?php if(isset($emailError)): ?>
+                    <p><?php echo $emailError; ?></p> 
+                <?php endif; ?>
                 <p class="pswd">wachtwoord</p>
                 <input type="password" name="password" id="password" placeholder="wachtwoord">
-
+                <?php if(isset($passwordError)): ?>
+                    <p><?php echo $passwordError; ?></p> 
+                <?php endif; ?>
                 </div>
                 <div class="formBtn2">
                     <input class="registerBtn" type="submit" value="registreren" name="register" id=""></input>
