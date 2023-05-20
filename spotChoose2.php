0b9b582f00393afe86779010ed2d0302699dfa2c
JarneSimons <r0901961@student.thomasmore.be>
2023-05-20 11:44:01 +0200

    feat: adding spotChoose2 page

---
+++ spotChoose2.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Document</title>
+</head>
+
+<body>
+    <div id="logo_zwart"></div>
+    <a class="back2" href="parking.php"><img src="img/back.png" alt=""></a>
+    <div class="options">
+        <a href="">traveller</a>
+        <a href="">parker</a>
+    </div>
+    <div class="title">
+        <h1>parking toevoegen</h1>
+    </div>
+    <div class="oprit2">
+        <img src="img/bedrijfsparking.png" alt="foto">
+        <h1>bedrijfsparking</h1>
+        <p>Ben je een onderneming dat momenteel de parking niet gebruikt, dan is de bedrijfsparking voor u de beste optie. Hier stel je de bedrijfsparking beschikbaar voor meerdere travellers. </p>
+    </div>
+    <div class="menu">
+        <a href="spotChoose.php"><img src="img/arrowL.png" alt="l"></a>
+        <div class="active"><a href=""></a></div>
+        <div class="inactive"><a href=""></a></div>
+        
+    </div>
+    
+    <div class="boxBtn">
+        <a class="btn" href="">bedrijfsparking kiezen</a>
+        
+    </div>
+
+
+    <?php include 'nav.php'; ?>
+
+</body>
+<script src="script.js"></script>
+
+</html>
