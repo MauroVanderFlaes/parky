78f8482ce8c6f1d0ee5039e46fb292a13518b17b
Mauro Van der Flaes <mauro.vanderflaes@gmail.com>
2023-06-13 01:55:59 +0200

    start van instellingen scherm

--- av.php
+++ av.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,25 +7,18 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/style.css">
-
-    <title>Parky - Instellingen </title>
+    <title>Parky - Algemene Voorwaarden</title>
 </head>
-
 <body>
-    <div class="logo">
+<div class="logo">
         <div id="logo_zwart"></div>
     </div>
     
-    <div class="buttons">
-    <h1>instellingen</h1>
-        <a href="av.php" class="settingBtn">algemene voorwaarden</a>
-        <a href="" class="settingBtn">faq</a>
-        <a href="" class="settingBtn">over de app</a>
-        <a href="" class="settingBtn">klantenservice</a>
-        <a href="" class="settingBtn">tutorial van de app</a>
+    <div class="av">
+    <h1>algemene voorwaarden</h1>
+
     </div>
 
     <?php include_once('nav.php'); ?>
 </body>
-
 </html>
