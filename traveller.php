4c880de1ed7fb3d3406343214fb12b869d09046e
Mauro Van der Flaes <mauro.vanderflaes@gmail.com>
2023-06-13 12:38:35 +0200

    traveller front end + styling

--- traveller.php
+++ traveller.php
@@ -1,10 +1,11 @@
-<?php 
+<?php
 
 include_once(__DIR__ . "/bootstrap.php");
 include_once(__DIR__ . "/LoginCheck.php");
 
 
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -18,18 +19,47 @@ include_once(__DIR__ . "/LoginCheck.php");
 </head>
 
 <body>
-    
-<div class="logo">
-    <div id="logo_zwart"></div>
-</div>
+
+    <div class="logo">
+        <div id="logo_zwart"></div>
+    </div>
     <div class="options">
         <a href="traveller.php" class="option-active">traveller</a>
         <a href="parking.php" class="option">parker</a>
     </div>
 
+    <div class="plaats">
+    <div class="sessie">
+        <h1>actieve parkeersessie</h1>
+        <div class="sessie-content">
+            <a href="index.php" class="add-spot">&plus;</a>
+            <h2 class="noposts">je hebt nog geen actieve parkeersessie</h2>
+        </div>
+
+    </div>
+    <div class="sessie">
+        <h1>actieve reservatie</h1>
+        <div class="sessie-content">
+            <a href="index.php" class="add-spot">&plus;</a>
+            <h2 class="noposts">je hebt nog geen actieve reservatie</h2>
+        </div>
+
+    </div>
+    <div class="sessie-end">
+        <h1>archief</h1>
+        <div class="sessie-content">
+            <a href="index.php" class="add-spot">&plus;</a>
+            <h2 class="noposts">je hebt nog niet geparkeerd. Er is geen archief</h2>
+        </div>
+
+    </div>
+
+    </div>
+
     
-    
-    <?php include_once('nav.php') ;?>
+
+
+    <?php include_once('nav.php'); ?>
     <!-- <script src="script.js"></script> -->
 </body>
 
